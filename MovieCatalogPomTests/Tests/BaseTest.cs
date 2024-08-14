eda6b7a5e783c95d99f9088803b25a5fa85aea93
EkaterinaGeorgieva <EkaterinaGeorgieva@KATERINA>
2024-08-14 11:22:57 +0300

    Add project files.

---
+++ MovieCatalogPomTests/Tests/BaseTest.cs
@@ -0,0 +1,62 @@
+﻿using MovieCatalogPomTests.Pages;
+using OpenQA.Selenium.Chrome;
+
+namespace MovieCatalogPomTests.Tests
+{
+    public class BaseTest
+    {
+        public IWebDriver driver;
+
+        public LoginPage loginPage;
+
+        public AddMoviePage addMoviePage;
+
+        public AllMoviesPage allMoviesPage;
+
+        public EditMoviePage editMoviePage;
+
+        public WatchedMoviesPage watchedMoviesPage;
+
+        public DeletePage deletePage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup() {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("profile.password_manager_enabled", false);
+            chromeOptions.AddArgument("--disable-search-engine-choice-screen");
+
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            loginPage = new LoginPage(driver);
+            addMoviePage = new AddMoviePage(driver);
+            allMoviesPage = new AllMoviesPage(driver);
+            editMoviePage = new EditMoviePage(driver);
+            watchedMoviesPage = new WatchedMoviesPage(driver);
+            deletePage = new DeletePage(driver);
+
+            loginPage.OpenPage();
+            loginPage.PerformLogin("test@testmail.com", "123456");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+
+        public string GenerateRandomTitle()
+        { 
+            var random = new Random();
+            return "TITLE: " + random.Next(10000, 100000);
+        }
+
+        public string GenerateRandomDescription()
+        {
+            var random = new Random();
+            return "DESCRIPTION: " + random.Next(10000, 100000);
+        }
+    }
+}
